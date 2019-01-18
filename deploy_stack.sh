de6c1f65c011821d0614a65c441f2e7e3354f50a
Chance Hudson <jchancehud@gmail.com>
2019-01-17 19:42:00 -0600

    Add stack deployment script

---
+++ deploy_stack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+STACK_NAME='writewei'
+COMPOSE_URL='https://raw.githubusercontent.com/writewei/infrastructure/master/docker-compose.yaml'
+
+docker stack rm $STACK_NAME
+sleep 20
+wget -N $COMPOSE_URL
+docker stack deploy --compose-file docker-compose.yaml $STACK_NAME
